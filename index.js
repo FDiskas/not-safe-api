079f502ce1cc808857af9874c6b8601c66530e91
fdiskas <projektas@gmail.com>
2024-04-03 03:05:57 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import isPorn from 'is-porn';
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+const isPornPromise = (url) => new Promise((resolve, reject) => {
+    isPorn(url, (error, status) => {
+        if (error) reject(error);
+        else resolve(status);
+    });
+});
+
+app.get('/', async (req, res, next) => {
+    const { site } = req.query;
+    let url = '';
+
+    try {
+        url = new URL(site).hostname;
+    } catch (error) {
+        return res.status(400).json({ error: 'Invalid url' });
+    }
+
+    try {
+        const status = await isPornPromise(url);
+        return res.json({ url, status });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Ready http://localhost:${port}`);
+});
